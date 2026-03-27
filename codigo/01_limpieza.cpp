3ddcb2dd8a2a87603947ecf897956e6ef39c3af7
Anthonny_Flores <thehoeless@gmail.com>
2026-03-27 13:26:24 -0600

    annade codigo para limpieza de datos

---
+++ codigo/01_limpieza.cpp
@@ -0,0 +1,51 @@
+// 01_limpieza.R
+// Limpieza y preparación de los datos crudos
+// Hecho por Anthonny Flores Rojas (C32975)
+
+// Librerias
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+
+//
+void chunksdetector() {
+
+}
+
+//
+void tochunks() {
+
+}
+
+//
+void toletters() {
+
+}
+
+//
+void datatotxt() {
+
+}
+
+//
+void txttodata() {
+
+}
+
+//
+void datacleaner() {
+
+}
+
+//
+int main() {
+    
+
+
+    return 0;
+
+}
