405f32d1b339c014e18d1416224d6fb26c4977bf
Anthonny_Flores <thehoeless@gmail.com>
2026-03-28 12:22:20 -0600

    docs: annade ideas para funciones

---
+++ codigo/funciones/conteos.cpp
@@ -0,0 +1,5 @@
+// conteos.cpp
+// Conteo para la creacion de las funciones de distribucion de la aparacion de los caracteres y numeros
+// Hecho por Anthonny Flores Rojas (C32975)
+// Tienen que ser una funcion dim() = 16 con 37 valores posibles (Se pueden annadir caracteres especiales)
+// CONTEO DE los primeros numeros de los chunks que sean numeros
---
+++ codigo/funciones/entropias.cpp
@@ -0,0 +1,5 @@
+// entropias.cpp
+// Calculo de entropias de las contrasennas
+// Hecho por Anthonny Flores Rojas (C32975)
+// Annadir la tipica shanno (la tengo en el repositorio de herramientas I)
+// Annadir otros tipos de entropias
