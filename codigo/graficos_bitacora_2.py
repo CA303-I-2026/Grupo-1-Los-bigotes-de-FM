9e180a15166e82a4646318060c9790568edd6bc1
Randal Picado <randal.picadobermudez@ucr.ac.cr>
2026-05-06 19:28:40 -0600

    Style: Se agrega la identidad visual

---
+++ codigo/graficos_bitacora_2.py
@@ -0,0 +1,24 @@
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# =========================
+# Estilo Bigotes
+# =========================
+
+# Estilo general
+sns.set_theme(style="ticks")
+
+# Paleta de colores
+sns.set_palette("Blues")
+
+# Configuración de tipografía
+plt.rcParams.update({
+    "axes.titlesize": 15,
+    "axes.labelsize": 13,
+    "xtick.labelsize": 13,
+    "ytick.labelsize": 13
+})
+
